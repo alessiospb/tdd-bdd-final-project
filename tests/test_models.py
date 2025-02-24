d46da8e2c2e0179e7876b46699f70f1bacab443f
Alexey <lelik.guru@gmail.com>
2025-02-24 05:29:17 -0500

    20250224-4

--- tests/test_models.py
+++ tests/test_models.py
@@ -201,3 +201,41 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.category, category)
+
+    def test_find_by_price(self):
+        """It should Find Products by Price"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        price = products[0].price
+        count = len([product for product in products if product.price == price])
+        found = Product.find_by_price(price)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.price, price)
+
+        found = Product.find_by_price("Foo")
+        with self.assertRaises(InvalidOperation) as context:
+            pfound = Product.find_by_price("Foo")
+        self.assertEqual(str(context.exception), "Invalid attribute: 123")
+
+    def test_deserialize_data_validation_error(self):
+        """ It should raise DataValidationError in deserialize """
+        product = ProductFactory()
+        product.id = None
+        product.available = "abc"
+        pjson = product.serialize()
+        with self.assertRaises(DataValidationError) as context:
+            product.deserialize(pjson)
+        self.assertEqual(str(context.exception), "Invalid type for boolean [available]: <class 'str'>")
+        product = ProductFactory()
+        pjson = product.serialize()
+        pjson["category"] = "123"
+        with self.assertRaises(DataValidationError) as context:
+            product.deserialize(pjson)
+        self.assertEqual(str(context.exception), "Invalid attribute: 123")
+        with self.assertRaises(DataValidationError) as context:
+            product.deserialize(None)
+        self.assertEqual(str(context.exception), "Invalid product: body of request contained bad or no data 'NoneType' object is not subscriptable")
+
+
